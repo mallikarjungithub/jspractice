89815644d6a02294e595908b854eaaf303225378
mallikarjungithub <mallikarjun327@gmail.com>
2019-09-19 12:25:05 +0530

    Jsfile

---
+++ js/all.js
@@ -0,0 +1,130 @@
+var a = 3;
+var b = "3";
+console.log(a == b);
+console.log(a === b);
+
+var a = 3 + 4 + "hai";
+console.log(a);
+
+var b = "hai" + 3 + 4;
+console.log(b);
+
+var test = 3 + 5 + "hai" + 8;
+console.log(test);
+
+var time = new Date();
+console.log(time.getTime());
+
+
+var Year = new Date();
+console.log(Year.getFullYear());
+
+var task = new Date();
+var month = ["jan", "feb", "mar", "apl", "may", "jun", "jly", "aug",
+  "sep", "oct", "nov", "dec"];
+console.log(month[task.getMonth("march")]);
+
+var d = new Date();
+var d1 = ["1", "2", "3", "18"];
+console.log(d1[d.getDate()]);
+
+var ab = 3;
+var cd = 5;
+console.log(ab + cd);
+console.log(ab - cd);
+console.log(ab * cd);
+console.log(ab/cd);
+console.log(ab%cd);
+
+
+
+for (i = 0; i < 101; i++) {
+  console.log(i);
+}
+for (i = 0; i < 101; i++) {
+  if (i % 2 == 0) {
+    console.log("even" + i);
+  }
+  else {
+    console.log("odd " + i);
+  }
+}
+var x=3;
+var y=4;
+console.log(x**y);
+
+var m=4;
+var n=3;
+console.log(m-=n);
+
+var name=("       mallikarjun        ");
+console.log(name.lenght);
+var result=name.trim();
+console.log(result);
+console.log(result.lenght);
+
+var o=3;
+var p=4;
+console.log(o&&p);
+
+var employee=[{
+    name:"malli",
+    age:"24",
+    mail:"malli327@gmail.com",
+},
+  {name:"mahi",
+    age:"24",
+    mail:"mahi327@gmail.com",
+  }
+]
+console.log(employee);
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
